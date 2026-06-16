e1a81252bd817b64e6389e117b5235c9f48e2b20
Vansh Namdev <engg.vansh1@gmail.com>
2026-06-16 13:15:15 +0530

    Total revenue
    solving the question of how much revenue generated from pizza sales.

---
+++ 04_total_revenue_generated_by_sales.sql
@@ -0,0 +1,9 @@
+-- Calculate the total revenue generated from pizza sales.
+
+use pizzahut;
+SELECT 
+    SUM(order_details.quantity * pizzas.price) AS total_revenue
+FROM
+    order_details
+        JOIN
+    pizzas ON order_details.pizza_id = pizzas.pizza_id;
